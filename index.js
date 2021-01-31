460f1d71538b608fcc4814ac040ace25b97f28e2
anthonyfinix <anthonyfinix@gmail.com>
2021-01-31 20:09:00 +0530

    made build folder public with html

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let express = require('express');
 let app = express();
 let port = process.env.PORT || 8080;
-
+app.use(express.static('build'))
 app.get('/',(req,res)=>{
-    res.send('hello');
+    res.sendFile('build/index.html');
 })
 app.listen(port,()=>{
     console.log('listening to port 3000');
