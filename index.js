ba2173a5ae2523626012cc36d09cc301b4491e8f
anthonyfinix <anthonyfinix@gmail.com>
2021-01-31 19:41:45 +0530

    added port

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let express = require('express');
 let app = express();
-
+let port = 3000;
 app.get('/',(res,req)=>{
     res.send('hello');
+})
+app.listen(port,()=>{
+    console.log('listening to port 3000');
 })
