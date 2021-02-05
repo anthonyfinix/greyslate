550cffeb21a1eb879d80e1f6ea064b1c5436214e
anthonyfinix <anthonyfinix@gmail.com>
2021-02-06 03:45:36 +0530

    hardcoded heroku domain name and port is from env var

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+var os = require("os");
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 const api = require('./api');
 app.use(cors());
 app.use(express.static('build'))
 app.get('/', (req, res) => {
     res.sendFile('build/index.html');
 })
+var hostname = os.hostname();
 app.use('/api', api);
 app.listen(port, () => {
     console.log(`listening to port ${port}`);
