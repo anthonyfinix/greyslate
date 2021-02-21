6226830edcf39395fa22252dc7521b6f6ec810a5
anthonyfinix <anthonyfinix@gmail.com>
2021-02-21 12:13:29 +0530

    react-router problem sorted

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var os = require("os");
+var path = require("path");
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -8,6 +8,9 @@ app.use(cors());
 app.use(express.static("build"));
 app.get("/", (req, res) => {
   res.sendFile("build/index.html");
+});
+app.get("/game/:gameId", (req, res) => {
+  res.sendFile( path.join(__dirname,"build/index.html"));
 });
 app.use("/api", api);
 app.listen(port, () => {
--- react-client/src/util/config.js
+++ react-client/src/util/config.js
@@ -1,4 +1,4 @@
 const config = {
-  api_url:"https://api.rawg.io",
+  api_url: process.env.DOMAIN_URL || "http://localhost:8080",
 };
 export default config;
